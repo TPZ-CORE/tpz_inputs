d18ad1930a8553157e1d9624a9928136638c5f05
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-12-04 12:34:41 +0200

    Update fxmanifest.lua

--- tpz_inputs/fxmanifest.lua
+++ tpz_inputs/fxmanifest.lua
@@ -6,12 +6,13 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE Inputs'
-version '1.0.2'
+version '1.0.3'
 
 ui_page 'html/index.html'
 
 client_scripts { 'client.lua' }
 
 files { 'html/**/*' }
+
 
 
